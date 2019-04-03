1eaf4916595b89460727cee0a3dc322fd24220d5
fibersel <0583463@gmail.com>
2019-04-03 16:48:30 +0300

    ws connection opened

--- src/pong/handlers.py
+++ src/pong/handlers.py
@@ -9,54 +9,29 @@ app = web.Application()
 queue = asyncio.Queue()
 
 
-async def merge_queues(common, session):
-    print("request merging made")
-
 async def matchmaker(queue):
-    ctr = 1
-    sess = []
     while True:
-        conn = await queue.get()
-        if ctr == 2:
-            sess.append(conn)
-            shared = asyncio.Queue()
-            ioloop = asyncio.get_event_loop()
-            tasks = [ioloop.create_task(session.handler(sess[0], sess[1]))]
-            sess = []
-            ctr = 1
-        else:
-            ctr += 1
-            print("appended")
-            sess.append(conn)
-
-
+        conn1 = await queue.get()
+        conn2 = await queue.get()
+        ioloop = asyncio.get_event_loop()
+        ioloop.create_task(session.handler(conn1, conn2))
+   
+    
 async def websocket_handler(request):
     print("kek2")
     ws = web.WebSocketResponse()
+    await ws.prepare(request)
     await queue.put(ws)
-   # await ws.prepare(request)
-
-   # async for msg in ws:
-   #     if msg.type == aiohttp.WSMsgType.TEXT:
-   #         print(msg.data)
-   #         if msg.data == 'close':
-   #             await ws.close()
-   #         else:
-   #             await ws.send_str(msg.data)
-   #     elif msg.type == aiohttp.WSMsgType.ERROR:
-   #         print('ws connection closed with exception %s' %
-   #               ws.exception())
-   # print('websocker connection closed')
     return ws
 
 
 async def start_background_tasks(app):
-    app['redis_listener'] = app.loop.create_task(matchmaker(queue))
+    app['matchmaker'] = app.loop.create_task(matchmaker(queue))
 
 
 async def cleanup_background_tasks(app):
-    app['redis_listener'].cancel()
-    await app['redis_listener']
+    app['matchmaker'].cancel()
+    await app['matchmaker']
 
 
 
