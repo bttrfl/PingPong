ae11ad297e4db4dc382a23be1d563ce7261541f5
bttrfl <snvalex@yandex.ru>
2019-04-27 22:52:23 +0300

    send game ready event

--- src/pong/session.py
+++ src/pong/session.py
@@ -5,12 +5,24 @@ import json
 
 #handles game sessions
 async def session_handler(client1, client2):
+    await start_game(client1, client2)
+
     loop = asyncio.get_event_loop()
     tasks = [
         loop.create_task(sync_events(client1, client2)),
         loop.create_task(sync_events(client2, client1)),
     ]
 
+    await asyncio.wait(*tasks)
+
+
+#sends events. indicating the game is ready to begin
+async def start_game(cleint1, client2):
+    loop = asycio.get_event_loop()
+    tasks = [
+        loop.create_task(client1.send_json({"event": "gameReady"})),
+        loop.create_task(client2.send_json({"event": "gameReady"})),
+    ]
     await asyncio.wait(*tasks)
 
 
@@ -32,4 +44,4 @@ async def sync_events(sender, reciever):
 
         #special event is sent in case of ws error
         elif msg.type == aiohttp.WSMsgType.ERROR:
-            loop.create_task(reciever.send_json({"event": "ws_error"}))
+            loop.create_task(reciever.send_json({"event": "wsError"}))
