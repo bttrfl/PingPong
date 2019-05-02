09f0bcfa14ffa98efa77ee9fb854e8830e6c09b1
bttrfl <snvalex@yandex.ru>
2019-05-03 01:18:33 +0300

    finished auth handlers and leaderboard handler

--- src/pong/handlers.py
+++ src/pong/handlers.py
@@ -4,6 +4,8 @@ from .session import session_handler
 from aiohttp import web
 import aiohttp_jinja2
 from aiohttp_session import get_session, new_session, invalidate, set_new_identity
+import json
+import hashlib
 
 
 __all__ = [
@@ -15,6 +17,7 @@ __all__ = [
 ]
 
 #a queue for incoming ws clients(game oponents)
+#TODO use a priority queue to match players with similar winrate/rating
 oponent_queue = asyncio.Queue()
 
 
@@ -47,33 +50,77 @@ async def matchmaker(queue):
 
 #returns a json serialized leaderboard data from database
 async def show_leaderboard(request):
-    pass
+    query = "SELECT * from leaderboard ORDER BY winrate DESC LIMIT 100"
+    db = request.app.db
+    async with db.cursor() as cur:
+        await cur.execute(query)
+        data = await cursor.fetchall()
+
+    json = json.dumps({"data": data})
+    return web.Response(text=json, content_type="application/json")
 
 
 #auth handlers
 async def signup_handler(request):
-    pass
+    data = await request.post()
+
+    #we first check if this user already exists in database
+    try:
+        uid = get_uid(data)
+    except KeyError:
+        return web.Response(status=400, text="Bad request")
+    if uid != None:
+        return web.Response(status=401, text="Unauthorized")
+
+    #add a new user to the database
+    query = "INSER INTO users_auth (name, password) VALUES (%(user)s, %(pwd)s)"
+    db = request.app.db
+    async with db.cursor() as cur:
+        #TODO use salt for password hashing
+        await cur.execue(query, {"user": data["user"], "pwd": sha256_hex(data["pwd"])})
+
+    await create_session(request, uid)
+
 
 async def login_handler(request):
     data = await request.post()
     try:
-        user = data["user"]
-        pwd = data["pwd"]
+        uid = get_uid(data)
     except KeyError:
         return web.Response(status=400, text="Bad request")
+
+    #user not found
+    if uid == None:
+        return web.Response(status=401, text="Unauthorized")
+
+    await create_session(request, uid)
+
+
+async def create_session(request, uid):
+    session = await new_session(request)
+    session.set_new_identity(uid)
+    session["user"] = user
+
+
+#gets userid from a database for a given username and password
+def get_uid(form_data):
+    user = form_data["user"]
+    pwd = form_data["pwd"]
 
     query = "SELECT id FROM users_auth WHERE name=%(user)s, AND password=%(pwd)s"
     db = request.app.db
     async with db.cursor() as cur:
-        await cur.execute(query, {"user": uname, "pwd": pwd})
+        #TODO use salt for password hashing
+        await cur.execute(query, {"user": user, "pwd": sha256_hex(pwd)})
         (uid,) = await cur.fetchone()
 
-    if uid == None:
-        return web.Response(status=403, text="Forbidden")
+    return uid
 
-    session = await new_session(request)
-    session.set_new_identity(uid)
-    session["user"] = user
+#returns a hex digest of a sha256 hash of a given string
+def sha256_hex(s):
+    m = hashlib.sha256()
+    m.update(bytes(s))
+    return m.hexdigest()
 
 
 async def logout_handler(request):
